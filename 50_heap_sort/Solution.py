1be287cbc360525512397d418a2db331cc338f8a
Alexander Scherbachenko <alexander.scherbachenko@dualbootpartners.com>
2020-05-05 20:18:21 +0400

    heap sort additional logic implemented.

--- 50_heap_sort/Solution.py
+++ 50_heap_sort/Solution.py
@@ -1,5 +1,25 @@
 import math
 
+def delete_item(heap, index):
+  last_index = len(heap) - 1
+  swap(heap, index, last_index)
+  heap.pop()
+
+  parent_index = parent(index)
+  if heap[parent_index] < heap[index]:
+    arise(heap, index)
+  else:
+    size = len(heap)
+    drown(heap, index, size)
+
+  return heap
+
+def arise(array, index):
+  parent_index = parent(index)
+  while parent_index in array and array[parent_index] < array[index]:
+    swap(array, index, parent_index)
+    arise(array, parent_index)
+
 def solution(numbers):
   heap = build_heap(numbers)
   size = len(numbers)
@@ -8,7 +28,7 @@ def solution(numbers):
   while i > 0:
     size = size - 1
     swap(numbers, 0, i)
-    drawn(numbers, 0, size)
+    drown(numbers, 0, size)
     i = i - 1
 
   return numbers
@@ -18,25 +38,28 @@ def build_heap(array):
   start_index = math.floor((size - 1) / 2) # the greatest element's index that has children
 
   while start_index >= 0:
-    drawn(array, start_index, size)
+    drown(array, start_index, size)
     start_index = start_index - 1
 
   return array
 
-def drawn(array, index, size):
-  left_index = left_child_index(index)
-  right_index = right_child_index(index)
-  largest_index = index
+def drown(array, index, size):
+  while index < size:
+    left_index = left_child_index(index)
+    right_index = right_child_index(index)
+    largest_index = index
 
-  if left_index < size and array[left_index] > array[largest_index]:
-    largest_index = left_index
+    if left_index < size and array[left_index] > array[largest_index]:
+      largest_index = left_index
 
-  if right_index < size and array[right_index] > array[largest_index]:
-    largest_index = right_index
+    if right_index < size and array[right_index] > array[largest_index]:
+      largest_index = right_index
 
-  if not largest_index == index:
+    if largest_index == index:
+      break
+
     swap(array, index, largest_index)
-    drawn(array, largest_index, size)
+    index = largest_index
 
 def swap(array, i1, i2):
   tmp = array[i1]
--- 50_heap_sort/TestHeapSort.py
+++ 50_heap_sort/TestHeapSort.py
@@ -1,9 +1,25 @@
 import unittest
 
-from Solution import solution
+from RecursiveSolution import recursive_solution
+from Solution import build_heap, delete_item, solution
 from Implementation import implementation
 
 class TestHeapSort(unittest.TestCase):
+  def test_delete_item(self):
+    heap = [10, 9, 5, 7, 8, 4, 1, 6, 3, 1, 3, 2, 2, 1, 1, 5]
+    actual = delete_item(heap, 14)
+    expected = [10, 9, 5, 7, 8, 4, 5, 6, 3, 1, 3, 2, 2, 1, 1]
+
+    self.assertEqual(actual, expected)
+    self.assertTrue(actual == build_heap(actual))
+
+    heap = [10, 9, 5, 7, 8, 4, 1, 6, 3, 1, 3, 2, 2, 1, 1, 5]
+    actual = delete_item(heap, 1)
+    expected = [10, 8, 5, 7, 5, 4, 1, 6, 3, 1, 3, 2, 2, 1, 1]
+
+    self.assertEqual(actual, expected)
+    self.assertTrue(actual == build_heap(actual))
+
   def test_solution(self):
     numbers_to_sort = [10, 9, 8, 7, 6, 5, 4, 3, 3, 2, 1, 1]
     expected = [1, 1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -11,12 +27,19 @@ class TestHeapSort(unittest.TestCase):
 
     self.assertEqual(actual, expected)
 
-  # def test_implementation(self):
-  #   numbers_to_sort = [10, 9, 8, 7, 6, 5, 4, 3, 3, 2, 1, 1]
-  #   expected = [1, 1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10]
-  #   actual = implementation(numbers_to_sort)
+  def test_recursive_solution(self):
+    numbers_to_sort = [10, 9, 8, 7, 6, 5, 4, 3, 3, 2, 1, 1]
+    expected = [1, 1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10]
+    actual = recursive_solution(numbers_to_sort)
 
-  #   self.assertEqual(actual, expected)
+    self.assertEqual(actual, expected)
+
+  def test_implementation(self):
+    numbers_to_sort = [10, 9, 8, 7, 6, 5, 4, 3, 3, 2, 1, 1]
+    expected = [1, 1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10]
+    actual = implementation(numbers_to_sort)
+
+    self.assertEqual(actual, expected)
 
 if __name__ == "__main__":
   unittest.main()
